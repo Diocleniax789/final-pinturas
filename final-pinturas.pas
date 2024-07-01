86361c206bf73c502b5a76c60e1b5ee5d06173be
Diocleniax789 <julioramdom123@gmail.com>
2024-07-01 18:06:28 -0300

    en la primera opcion de carga de pinturas, se ha creado una funcion para verificar si el archivo pinturas esta vacio. por otra parte tambien se ha terminado la primera parte de la condicional por si este esta vacio, se agrega un primer registro, esto nos permite luego poder verificar si al ingresar un segundo registro, este no se encuentra repetido

--- final-pinturas.pas
+++ final-pinturas.pas
@@ -6,7 +6,7 @@ TYPE
     pinturas = RECORD
             codp: integer;
             descripcion: string;
-            pre: array[0..3]of real;
+            precio: array[0..3]of real;
             preparado: char;
             stock: array [0..3]of integer;
             END;
@@ -50,34 +50,130 @@ PROCEDURE crea_archivo_pedidos;
  close(archivo_pedidos);
  END;
 
+FUNCTION verifica_tamanio_archivo_pinturas(): boolean;
+ BEGIN
+ reset(archivo_pinturas);
+ IF filesize(archivo_pinturas) = 0 THEN
+  verifica_tamanio_archivo_pinturas:= true
+ ELSE
+  verifica_tamanio_archivo_pinturas:= false;
+ close(archivo_pinturas);
+ END;
+
+FUNCTION valida_envase(): integer;
+VAR
+ op: integer;
+ BEGIN
+  REPEAT
+  writeln('ENVASES');
+  writeln('-------');
+  writeln();
+  writeln('0. 1 Litro');
+  writeln('1. 4 Litros');
+  writeln('2. 10 Litros');
+  writeln('3. 20 Litros');
+  writeln();
+  write('Seleccione tipo de envase: ');
+  readln(op);
+  IF (op < 0) OR (op > 3) THEN
+   writeln('VALOR INCORRECTO. INGRESE NUEVAMENTE');
+  UNTIL (op >= 0) AND (op <= 3);
+ valida_envase:= op;
+ END;
+
+PROCEDURE carga_pinturas;
+VAR
+ env,op,op1,f: integer;
+ BEGIN
+ REPEAT
+ IF verifica_tamanio_archivo_pinturas = true THEN
+  BEGIN
+  reset(archivo_pinturas);
+  writeln('CARGA DE DATOS DE PINTURA');
+  writeln('-------------------------');
+  writeln();
+  write('>>> Ingrese codigo de pintura: ');
+  readln(registro_pinturas.codp);
+  writeln();
+  write('>>> Ingrese descripcion de pintura: ');
+  readln(registro_pinturas.descripcion);
+  writeln();
+  env:= valida_envase;
+  writeln();
+  write('>>> Ingrese el precios: ');
+  FOR f:= 0 TO 3 DO
+   BEGIN
+   writeln('Precio ',f);
+   writeln();
+   write('Ingrese precio: ');
+   readln(registro_pinturas.precio[f]);
+   END;
+  writeln();
+  REPEAT
+  write('>>> Es preparada[s/n]?: ');
+  readln(op1);
+  IF (op1 <> 's') AND (op1 <> 'n') THEN
+   writeln('VALOR INCORRECTO. INGRESE NUEVAMENTE');
+  UNTIL (op1 = 's') OR (op1 = 'n');
+  registro_pinturas.preparado:= op1;
+  writeln();
+  write('>>> Ingrese el stock: ');
+  FOR f:= 0 TO 3 DO
+   BEGIN
+    writeln('Stock',f);
+    writeln();
+    write('Ingrese Stock: ');
+    readln(registro_pinturas.stock[f]);
+   END;
+  seek(archivo_pinturas,filesize(archivo_pinturas));
+  write(archivo_pinturas, registro_pinturas);
+  close(archivo_pinturas);
+  writeln();
+  writeln('===========================================');
+  writeln('*** SE HA CARGADO EL REGISTRO CON EXITO ***');
+  writeln('===========================================');
+  delay(2000);
+  END
+ ELSE
+
+ UNTIL
+ END;
+
+
+
 PROCEDURE menu_principal;
 VAR
    opcion: integer;
    BEGIN
    REPEAT
    clrscr;
-   writeln('1. Generar pedido');
-   writeln('2. Actualizar precios');
-   writeln('3. Listado de pinturas preparadas');
-   writeln('4. Salir');
+   writeln('1. Carga pinturas');
+   writeln('2. Generar pedido');
+   writeln('3. Actualizar precios');
+   writeln('4. Listado de pinturas preparadas');
+   writeln('5. Salir');
    writeln();
    writeln('Seleccione una opcion: ');
    readln(opcion);
    CASE opcion OF
     1:BEGIN
       clrscr;
-      genera_pedido;
+      carga_datos;
       END;
     2:BEGIN
       clrscr;
-      actualiza_precio;
+      genera_pedido;
       END;
     3:BEGIN
+      clrscr;
+      actualiza_precio;
+      END;
+    4:BEGIN
       clrscr;
       listar_pinturas
       END;
    END;
-   UNTIL (opcion = 4);
+   UNTIL (opcion = 5);
    END;
 
 BEGIN
